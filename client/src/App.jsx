c7d8f179e657dea5f7b9d8a966376bad56d27396
Aman Singh <amanksofficial@gmail.com>
2022-08-07 15:04:10 +0530

    url added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { VscSearch, VscClose } from "react-icons/vsc";
 import axios from "axios";
 import { Card } from "./component/Card";
@@ -53,70 +53,91 @@ function App() {
   };
 
   return (
-    <div className="App text-slate-900">
-      <div className="container mx-auto h-[95vh] w-11/12 mb-4">
-        <h1 className="text-3xl md:text-5xl font-semibold md:pt-10 pt-5 md:pl-5">
-          🖇 Link <span className="text-red-500"> Previewer</span>
-        </h1>
-        <div className="flex flex-col md:flex-row gap-3 md:mt-4 md:items-center py-4 md:pl-5">
-          <div className="w-full md:w-[32rem] max-w-[100%] relative">
-            <VscSearch className="absolute z-10 left-5 top-1/3 text-slate-500" />
-            <input
-              value={input}
-              onChange={(e) => {
-                setInput(e.target.value);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  handleButtonClick();
-                }
-              }}
-              className="placeholder:italic py-3 px-12 drop-shadow focus:drop-shadow-none outline-red-500 rounded-full border w-full accent-red-500 text-slate-500"
-              type="text"
-              placeholder="Example - https://www.findcoder.io/"
-            />
-            {input ? (
-              <VscClose
-                onClick={() => {
-                  setInput("");
+    <Fragment>
+      <div className="App text-slate-900 pb-32">
+        <div className="container mx-auto h-[95vh] w-11/12 mb-4 relative">
+          <h1 className="text-3xl md:text-5xl font-semibold md:pt-10 pt-5 md:pl-5">
+            🖇 Link <span className="text-red-500"> Previewer</span>
+          </h1>
+          <div className="flex flex-col md:flex-row gap-3 md:mt-4 md:items-center py-4 md:pl-5">
+            <div className="w-full md:w-[32rem] max-w-[100%] relative">
+              <VscSearch className="absolute z-10 left-5 top-1/3 text-slate-500" />
+              <input
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
                 }}
-                className="absolute z-10 right-5 top-1/3 text-slate-500 text-xl cursor-pointer"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleButtonClick();
+                  }
+                }}
+                className="placeholder:italic py-3 px-12 drop-shadow focus:drop-shadow-none outline-red-500 rounded-full border w-full accent-red-500 text-slate-500"
+                type="text"
+                placeholder="Example - https://www.findcoder.io/"
+              />
+              {input ? (
+                <VscClose
+                  onClick={() => {
+                    setInput("");
+                  }}
+                  className="absolute z-10 right-5 top-1/3 text-slate-500 text-xl cursor-pointer"
+                />
+              ) : (
+                ""
+              )}
+            </div>
+            <button
+              disabled={loading}
+              onClick={handleButtonClick}
+              className="px-3 py-2 bg-red-500 text-white hover:bg-red-700 duration-300 rounded disabled:bg-gray-500 disabled:text-gray-100"
+            >
+              Generate ⚙️
+            </button>
+          </div>
+          <div className="md:p-5 relative">
+            {loading ? <DummyCard /> : ""}{" "}
+            {metadata && !loading ? (
+              <Card
+                img={metadata.image}
+                link={metadata.url.slice(0, 60) + "..."}
+                title={metadata.title}
+                description={
+                  metadata.description
+                    ? metadata.description.slice(0, 100) + "..."
+                    : "No description found"
+                }
               />
             ) : (
               ""
             )}
           </div>
-          <button
-            disabled={loading}
-            onClick={handleButtonClick}
-            className="px-3 py-2 bg-red-500 text-white hover:bg-red-700 duration-300 rounded disabled:bg-gray-500 disabled:text-gray-100"
-          >
-            Generate ⚙️
-          </button>
+          <div className="hidden md:block bg-gray-100 py-4 px-8 rounded-lg max-w-[32rem] drop-shadow-sm absolute right-20 top-64">
+            <h3 className="font-semibold text-xl">Instructions</h3>
+            <ul>
+              <li>
+                - Please enter complete URL <br />{" "}
+                <span className="ml-8 italic text-sm">
+                  {" "}
+                  https://www.google.com
+                </span>{" "}
+                ✅ <br />
+                <span className="ml-8 italic text-sm">
+                  {" "}
+                  https://google.com
+                </span>{" "}
+                ❌ <br />
+                <span className="ml-8 italic text-sm">google.com</span> ❌
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="md:p-5">
-          {loading ? <DummyCard /> : ""}{" "}
-          {metadata && !loading ? (
-            <Card
-              img={metadata.image}
-              link={metadata.url.slice(0, 60) + "..."}
-              title={metadata.title}
-              description={
-                metadata.description
-                  ? metadata.description.slice(0, 100) + "..."
-                  : "No description found"
-              }
-            />
-          ) : (
-            ""
-          )}
-        </div>
+        <Toast {...toast} />
       </div>
-      <footer className="text-center border-t z-10 p-2 sticky bottom-0 bg-white">
+      <footer className="text-center w-full border-t z-10 p-2 sticky bottom-0 bg-white">
         Made with 😴 by <strong className="text-red-500">Aman</strong>
       </footer>
-      <Toast {...toast} />
-    </div>
+    </Fragment>
   );
 }
 
